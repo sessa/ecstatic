435cee71821ab089f4e75f61552fb930985e5a9c
mweiss17 <martin.weiss@mail.mcgill.ca>
2015-08-01 17:54:52 -0400

    working socket connection

--- js/controllers.js
+++ js/controllers.js
@@ -10,23 +10,27 @@ angular.module('controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-  $scope.create_room = function() {
-    console.log("hello");
-    Chats.create_room();
-  };
+    $scope.create_room = function() {
+        Chats.create_room();
+    };
+    
+    Chats.getRooms().then(function(messages) {
+        console.log("in");
+      $scope.chats = messages;
+    });
 
-  $scope.chats = Chats.rooms();
 
   // refresh the channels list
   $scope.doRefresh = function() {
+	console.log("heyo");
+		console.log($scope.chats);
 
-    //tell the ionScroll that the job is done
-    $scope.$broadcast('scroll.refreshComplete');
-
+	//tell the ionScroll that the job is done
+	$scope.$broadcast('scroll.refreshComplete');
   };
 
   $scope.remove = function(chat) {
-    Chats.remove(chat);
+	Chats.remove(chat);
   };
 
 
@@ -38,86 +42,86 @@ angular.module('controllers', [])
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
-    enableFriends: true
+	enableFriends: true
   };
 })
 
 //the Main Controller, gets info from server, updates scope for index.jade
 .controller('mainController',['$scope', '$http', function($scope, $http) {
 
-        //query index.js
-        $http.get('/api/upcomingEvents')
-        .success(function(data) {
+		//query index.js
+		$http.get('/api/upcomingEvents')
+		.success(function(data) {
 
-            //parse the data
-            $scope.upcomingEvents = data;
-            $scope.server_playlist = data.playlist;
-           
-            //call this function every second
-            countdown(data.start_time, function(ts){
-                console.log("ts.value="+ts.value);
-                
-                //every second
-                if(!$scope.$$phase) {
-                  console.log("$scope.trackIndex = trackIndex;"+trackIndex);
-                  $scope.trackIndex = trackIndex;
-                  $scope.$apply() 
-                }
+			//parse the data
+			$scope.upcomingEvents = data;
+			$scope.server_playlist = data.playlist;
+		   
+			//call this function every second
+			countdown(data.start_time, function(ts){
+				console.log("ts.value="+ts.value);
+				
+				//every second
+				if(!$scope.$$phase) {
+				  console.log("$scope.trackIndex = trackIndex;"+trackIndex);
+				  $scope.trackIndex = trackIndex;
+				  $scope.$apply() 
+				}
 
-                //if there are less than 2 seconds left before the event, hide the join button
-                if(ts.value < -2000){
-                  $scope.showButton = "hidden";
-                }
+				//if there are less than 2 seconds left before the event, hide the join button
+				if(ts.value < -2000){
+				  $scope.showButton = "hidden";
+				}
 
-                //if the event has not started, hide a label and button 
-                else{
-                  $scope.currentlyPlaying = "hidden";
-                  $scope.showButton = "asdfasdfsdf";
-                }
+				//if the event has not started, hide a label and button 
+				else{
+				  $scope.currentlyPlaying = "hidden";
+				  $scope.showButton = "asdfasdfsdf";
+				}
 
-                //update the countdown every second
-                //document.getElementById('seconds').innerHTML = ts.seconds.toString();
-                //document.getElementById('minutes').innerHTML = ts.minutes.toString();
-                //document.getElementById('hours').innerHTML = ts.hours.toString();
-              }, 
-          countdown.HOURS | countdown.MINUTES | countdown.SECONDS, 3);
-      })
-      .error(function(data) {
-          console.log('Error: ' + data);
-      });
+				//update the countdown every second
+				//document.getElementById('seconds').innerHTML = ts.seconds.toString();
+				//document.getElementById('minutes').innerHTML = ts.minutes.toString();
+				//document.getElementById('hours').innerHTML = ts.hours.toString();
+			  }, 
+		  countdown.HOURS | countdown.MINUTES | countdown.SECONDS, 3);
+	  })
+	  .error(function(data) {
+		  console.log('Error: ' + data);
+	  });
 
-      //query index.js
-      $http.get('/api/sync')
-      .success(function(data) {
-          timeOfReturn = new Date().getTime();
-          var json = JSON.parse(data);
-          console.log("http.get('/app/api/sync'), timeOfReturn = "+ timeOfReturn);
-          console.log("http.get('/app/api/sync'), json = "+ data);
-          if(json.elapsedTime > 0){
-            elapsed = json.elapsedTime;
+	  //query index.js
+	  $http.get('/api/sync')
+	  .success(function(data) {
+		  timeOfReturn = new Date().getTime();
+		  var json = JSON.parse(data);
+		  console.log("http.get('/app/api/sync'), timeOfReturn = "+ timeOfReturn);
+		  console.log("http.get('/app/api/sync'), json = "+ data);
+		  if(json.elapsedTime > 0){
+			elapsed = json.elapsedTime;
 
-            //FOR GETTING THE ACTUAL TRACK INDEX
-            trackIndex = json.trackIndex;
+			//FOR GETTING THE ACTUAL TRACK INDEX
+			trackIndex = json.trackIndex;
 
-            // updates the playlist's selected track
-            console.log("trackIndex="+trackIndex);
-            $scope.trackIndex = trackIndex;
+			// updates the playlist's selected track
+			console.log("trackIndex="+trackIndex);
+			$scope.trackIndex = trackIndex;
 
-            //FOR TESTING
-            //trackIndex = 1;
-          }
-          else{
-            console.log("event hasn't started");
-          }
-      })
-      .error(function(data) {
-          console.log('Error: ' + data);
-      });
+			//FOR TESTING
+			//trackIndex = 1;
+		  }
+		  else{
+			console.log("event hasn't started");
+		  }
+	  })
+	  .error(function(data) {
+		  console.log('Error: ' + data);
+	  });
 
-    $scope.update_selected_track = function(index){
-        $scope.trackIndex = index;
-        if(!$scope.$$phase) {
-          $scope.$apply() 
-        }
-    }
+	$scope.update_selected_track = function(index){
+		$scope.trackIndex = index;
+		if(!$scope.$$phase) {
+		  $scope.$apply() 
+		}
+	}
 }]);
--- js/services.js
+++ js/services.js
@@ -6,29 +6,72 @@ angular.module('services', ['btford.socket-io'])
   });
 })
 
-.factory('Chats', function (socket) {
+.factory('Chats', ['$q', '$rootScope', 'socket', function($q, $rootScope, socket) {
+    // We return this object to anything injecting our service
+    var Service = {};
+    // Keep all pending requests here until they get responses
+    var callbacks = {};
+    // Create a unique callback ID to map requests to responses
+    var currentCallbackId = 0;
+    // Create our websocket object with the address to the websocket
+    
+	socket.on('rooms', function (data) {
+		listener(data);
+	});
 
-	// Some fake testing data
-	var chats = [{
-		id: 0,
-		name: 'Silent Disco Squad',
-		lastText: 'Ready to groove?',
-		face: 'https://goo.gl/Jix8qP',
-		url: 'https://soundcloud.com/kizma/rmfstar-kizma-takethemountain-silent-disco-mix'
-	}, {
-		id: 1,
-		name: 'Sunset Warriors',
-		lastText: '',
-		face: 'http://goo.gl/6EzYGo'
-	}];
+    function sendRequest(request) {
+	      var defer = $q.defer();
+	      var callbackId = getCallbackId();
+	      callbacks[callbackId] = {
+	        time: new Date(),
+	        cb:defer
+	      };
+	      request.callback_id = callbackId;
+	      console.log('Sending request', request);
+	      socket.emit(request.msg);
+      return defer.promise;
+    }
+
+    function listener(data) {
+      var messageObj = data;
+      console.log("Received data from websocket: ", messageObj);
+      // If an object exists with callback_id in our callbacks object, resolve it
+      if(callbacks.hasOwnProperty(messageObj.callback_id)) {
+        console.log("hello: ");
+        console.log(callbacks[messageObj.callback_id]);
+        $rootScope.$apply(callbacks[messageObj.callback_id].cb.resolve(messageObj.data));
+        delete callbacks[messageObj.callbackID];
+      }
+    }
+    // This creates a new callback ID for a request
+    function getCallbackId() {
+      currentCallbackId += 1;
+      if(currentCallbackId > 10000) {
+        currentCallbackId = 0;
+      }
+      return currentCallbackId;
+    }
+
+    // Define a "getter" for getting customer data
+    Service.getRooms = function() {
+      var request = {
+        msg: "rooms"
+      }
+      // Storing in a variable for clarity on what sendRequest returns
+      var promise = sendRequest(request); 
+      return promise;
+    }
+
+    return Service;
+}]);
+
+/*.factory('Chats', [ function (socket) {
 
 	return {
 		rooms: function() {
-			 //gimme the rooms
-
 			socket.emit('rooms');
 			socket.on('rooms', function (data) {
-                console.log("data="+JSON.stringify(data));
+				data.forEach(function(chat) { console.log(chat.room_id); });
                 return data;
             });
 		},
@@ -43,4 +86,4 @@ angular.module('services', ['btford.socket-io'])
             });
 		}
 	};
-});
+}]);*/
