e7b2b2a04a34c07b7b06335fc08c7813a8ec9b96
mweiss17 <martin.weiss@mail.mcgill.ca>
2015-08-28 19:00:12 -0400

    removed superfluous files

--- www/player/video.module.js
+++
@@ -1,8 +0,0 @@
-angular.module('ecstatic.video', [
-	'ecstatic.channels',
-	'ecstatic.sockets',       
-	'com.2fdevs.videogular',
-    'com.2fdevs.videogular.plugins.controls',
-    'com.2fdevs.videogular.plugins.overlayplay',
-    'com.2fdevs.videogular.plugins.poster'
-])
