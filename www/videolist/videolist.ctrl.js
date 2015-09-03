2c4af92d640130beecf65b6610566b16bfe4d23f
mweiss17 <martin.weiss@mail.mcgill.ca>
2015-09-03 15:49:38 -0400

    videolist shows all the videos

--- www/videolist/videolist.ctrl.js
+++ www/videolist/videolist.ctrl.js
@@ -1,14 +1,25 @@
 angular.module('ecstatic.videolist')
 
 .controller('videolistCtrl',
-	['channelServices', '$stateParams', '$scope', '$state', 'playerServices', function(channelServices, $stateParams, $scope, $state, playerServices) {
-	$scope.goToVideolist = function(){
-		$state.go('tab.channels-videolist');
-	}
+	['$sce', 'channelServices', '$stateParams', '$scope', '$state', 'playerServices', function($sce, channelServices, $stateParams, $scope, $state, playerServices) {
     channelServices.getChannels().then(function (channels){
-    	console.log("playerServices.channel_id"+playerServices.channel_id);
-        $scope.channel = channelServices.getChannel(playerServices.channel_id);
-		console.log("channel.cliplist"+JSON.stringify(channel.cliplist));
+
+        var channel = channelServices.getChannel(playerServices.channel_id);
+        $scope.cliplist = [];
+
+        for (var i = 0; i < channel.cliplist.length; i++){
+        	var source = channel.cliplist[i];
+        	var type = "video/" + source.format;
+        	var cliplistDict = 
+        		{src: $sce.trustAsResourceUrl("https://s3.amazonaws.com/ecstatic-videos/"+source.video_key), 
+        		type: type,
+        		video_key: source.video_key
+        		};
+
+        	$scope.cliplist.push(cliplistDict);
+        }
+
+		console.log("channel.cliplist"+JSON.stringify($scope.cliplist));
 		console.log("channel"+JSON.stringify(channel));		
 	});
 }])
