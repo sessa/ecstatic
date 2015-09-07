f63436cc8ee3c25d1b01fe3941dada1e3c7ebec7
David Hernon <david.hernon1@gmail.com>
2015-09-07 18:41:12 -0400

    added text feedback for mobile user adding videos. needs styling and then pull request

--- www/videoplayer/videoplayer.ctrl.js
+++ www/videoplayer/videoplayer.ctrl.js
@@ -8,6 +8,16 @@ angular.module('ecstatic.videoplayer')
         $scope.render();
     });
 
+    $scope.videoMessage = "no video yet :(";
+    $scope.onDesktop;
+    if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
+        console.log("we are not in browser");
+        $scope.onDesktop = false;
+    } else {
+        console.log("we are in browser");
+        $scope.onDesktop = true;
+    }
+
     $scope.render = function() {
         channelServices.getChannels().then(function (channels){
             $scope.videoplayerServices = videoplayerServices;
@@ -18,7 +28,11 @@ angular.module('ecstatic.videoplayer')
             if($scope.cliplistLength !== 0){
                 console.log("render cliplist");
                 videoplayerServices.setChannel(channel);
-                $scope.showVideoplayer = true;
+                if($scope.onDesktop){
+                    $scope.showVideoplayer = true;
+                }else{
+                    $scope.videoMessage = "Video Playback Support coming to Mobile Soon :)";
+                }
                 $scope.videoplayerServices = videoplayerServices;
             }
         });
