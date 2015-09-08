b241e4360b6a8f7800b87677066273a6786d1b27
mweiss17 <martin.weiss@mail.mcgill.ca>
2015-09-08 15:58:51 -0400

    changed socket

--- www/sockets/sockets.services.js
+++ www/sockets/sockets.services.js
@@ -2,11 +2,13 @@ angular.module('ecstatic.sockets')
 
 .factory('socket', ['socketFactory','ConfigService', '$location', function (socketFactory, ConfigService, $location) {
 	return socketFactory({
-		ioSocket: io.connect($location.absUrl().split(':')[0]+ ':' + $location.absUrl().split(':')[1] +':3001')
+		ioSocket: io.connect($location.absUrl().split('/')[0]+ '//' + $location.absUrl().split('/')[2])
 	});
 }])
 
 .factory('socketManager', ['$q', '$rootScope', 'socket', '$location', function($q, $rootScope, socket, $location) {
+		console.log("socketManager"+$location.absUrl().split(':')[0]+ ':' + $location.absUrl().split(':')[1] +':3001');
+		console.log("socketmaaaanager"+$location.absUrl().split('/')[0]+ '//' + $location.absUrl().split('/')[2]);
 		// We return this object to anything injecting our service
 		var Service = {};
 		// Keep all pending requests here until they get responses
