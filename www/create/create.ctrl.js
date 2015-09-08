fb69b0fad3c877eff7a54033abfcd880bbcb8943
mweiss17 <martin.weiss@mail.mcgill.ca>
2015-09-08 15:21:55 -0400

    another try

--- www/create/create.ctrl.js
+++ www/create/create.ctrl.js
@@ -16,16 +16,18 @@ angular.module('ecstatic.create')
 		var milliSinceEpoch = new Date().getTime();
 		var starterTime = (hours*3600000+mins*60000)+milliSinceEpoch;
 
-		$ionicLoading.show({
-			template: '<ion-spinner icon="android" class="spinner-light"></ion-spinner>'
-		});
+		// $ionicLoading.show({
+		// 	template: '<ion-spinner icon="android" class="spinner-light"></ion-spinner>'
+		// });
+		console.log("before create");
+		console.log("before create+$stateParams.channelName"+$stateParams.channelName);
 		channelServices.createChannel($stateParams.channelName, starterTime).then(function(data) {
-			console.log("bang")
-			$ionicLoading.hide();
-		    $ionicHistory.nextViewOptions({
-		      disableAnimate: false,
-		      disableBack: true
-		    });
+			console.log("within create");
+			// $ionicLoading.hide();
+		 //    $ionicHistory.nextViewOptions({
+		 //      disableAnimate: false,
+		 //      disableBack: true
+		 //    });
             $state.go('channel', {channel_id:data.player_state.channel_id});
     	});
 	}
