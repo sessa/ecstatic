06ec03e57767f10cff260ad656eb0d2d85b21740
mweiss17 <martin.weiss@mail.mcgill.ca>
2015-09-14 11:58:42 -0400

    trying to get jade to autoupdate

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,15 @@ var paths = {
 
 //compile Jade files to html
 gulp.task('jade', function (done) {
+	console.log("asdf");
   	gulp.src(paths.jade)
     .pipe(jade())
     .pipe(gulp.dest('www'))
     .on('end', done);
+});
+
+gulp.task('watch', function() {
+	gulp.watch(paths.jade, ['jade']);
 });
 
 gulp.task('default', ['jade']);
